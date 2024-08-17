8323b46bcb247dd78e81ceac92e13592e7e51c2f
vic-001 <victorhugosparapam@gmail.com>
2024-08-17 13:41:46 +0000

    aaaaaaaa

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ if 1 <= numero <= 10:
   for i in range(1, 11):
     print(f"{numero} x {i} = {numero * i}")
   else:
-    print("numero invaido. digite um numero de 1 a 10")
+    print("numero invaido. digite um numero de 2 a 10")
