3e72c76132a838cad5a683e9fe332e7b655e939b
vic-001 <victorhugosparapam@gmail.com>
2024-08-17 13:39:59 +0000

    teste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numero = int(input("digiite um numero de 1 a 10: ") )
+numero = int(input("digiite um numero de 1 a 11: "))
 if 1 <= numero <= 10:
-  for i in range(1,11):
+  for i in range(1, 11):
     print(f"{numero} x {i} = {numero * i}")
   else:
     print("numero invaido. digite um numero de 1 a 10")
